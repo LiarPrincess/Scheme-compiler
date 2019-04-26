e4d044f0ebca998bd4ad10b47be453bc0220916b
Michal <michalmat@users.noreply.github.com>
2019-04-26 17:07:16 +0200

    Parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,28 @@
 module Main where
+
+import Lib
 import System.Environment
 
 main :: IO ()
 main = do
-  args <- getArgs
-  putStrLn ("Hello, " ++ args !! 0)
+  (expr:_) <- getArgs
+  putStrLn "(a test)"
+  putStrLn (myParse "(a test)")
+  putStrLn ""
+
+  putStrLn "(a (nested) test)"
+  putStrLn (myParse "(a (nested) test)")
+  putStrLn ""
+
+  putStrLn "(a (dotted . list) test)"
+  putStrLn (myParse "(a (dotted . list) test)")
+  putStrLn ""
+
+  putStrLn "(a '(quoted (dotted . list)) test)"
+  putStrLn (myParse "(a '(quoted (dotted . list)) test)")
+  putStrLn ""
+
+  putStrLn "(a '(imbalanced parens)"
+  putStrLn (myParse "(a '(imbalanced parens)")
+  putStrLn ""
+
